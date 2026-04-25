4405ebf6680bb1e19e8a29d7c4e4155466f17ff4
Amrit Mahajan <44335666+Lightning-Nemesis@users.noreply.github.com>
2026-04-25 12:31:02 -0700

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  base: '/data-scientist-portfolio/',
+  base: '/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
